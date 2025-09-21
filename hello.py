7f5a0b6171e80d0f586bd6f2ec44eb761661dd80
Lynne <lynne.kailin.liu@gmail.com>
2025-09-20 21:38:18 -0400

    Activity 1.4; Follow example in chapter 4; Click Home for user input, no time shows (erased since not says to keep in handout).

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 from datetime import datetime
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask import session, flash, redirect, url_for
+
+try:
+    from wtforms.fields import EmailField          # WTForms 3.x
+except ImportError:
+    from wtforms.fields.html5 import EmailField    # WTForms 2.x
 
 # ensure bootstrap is installed
 try:
@@ -10,16 +19,44 @@ except ImportError:
 
 app = Flask(__name__)
 
+# wtf
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+# form input
+class NameEmailForm(FlaskForm):
+    name  = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+
 # init ext
 moment = Moment(app)
 if Bootstrap:
     bootstrap = Bootstrap(app)
 
 # main /index share same vision，and pass in current_time
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = email = is_uoft = None
+    form = NameEmailForm()
+    if form.validate_on_submit():
+        name = (form.name.data or '').strip()
+        email = (form.email.data or '').strip()
+        is_uoft = 'utoronto' in email.lower()
+
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name']   = name
+        session['email']  = email
+        session['is_uoft'] = is_uoft
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), is_uoft=session.get('is_uoft'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
